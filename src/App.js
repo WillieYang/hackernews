1f3eedf9e249bbeeb4794172150b7810ba3454ff
yangs <yangs@gisuni.com>
2018-05-06 22:30:27 +0800

    Remove the .idea folder, and Complex JavaScript in JSX

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/react.js/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
 class App extends Component {
   render() {
     const helloWorld = "Welcome to the World of React";
@@ -16,6 +35,20 @@ class App extends Component {
         </header>
         <h2>{ helloWorld }</h2>
         <p>{username.firstName} {username.lastName}</p>
+        {
+          list.map(function(item) {
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
