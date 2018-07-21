3eb2670c809dac87fdab9fb51d32a8f132f853a6
WillieYang <849346604@qq.com>
2018-07-21 18:00:25 +0800

    Convert the ES6 class component into functioanl stateless component (Component: Search)

--- src/App.js
+++ src/App.js
@@ -94,20 +94,29 @@ class App extends Component {
   }
 }
 
-class Search extends  Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        { children } <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-      );
-  }
-}
+// class Search extends  Component {
+//   render() {
+//     const { value, onChange, children } = this.props;
+//     return (
+//       <form>
+//         { children } <input
+//           type="text"
+//           value={value}
+//           onChange={onChange}
+//         />
+//       </form>
+//       );
+//   }
+// }
+
+const Search = ({ value, onChange, children}) =>
+  <form>
+    { children } <input
+    type="text"
+    value={value}
+    onChange={onChange}
+  />
+  </form>
 
 class Table extends Component {
   render() {
@@ -123,16 +132,29 @@ class Table extends Component {
             <span>{ item.num_comments }</span>
             <span>{ item.points }</span>
             <span>
-              <button
+              <Button
                 onClick={() => onDismiss(item.objectID)}
-                type="button"
               >
                 Dismiss
-              </button>
+              </Button>
             </span>
           </div>
         )}
       </div>
+    );
+  }
+}
+
+class Button extends Component{
+  render() {
+    const { onClick, className = '', children } = this.props;
+    return (
+      <button
+        onClick = {onClick}
+        className={className}
+      >
+        {children}
+      </button>
     );
   }
 }
