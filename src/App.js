9c9d6c0883124f74667b4e630430223d48e89a71
WillieYang <849346604@qq.com>
2018-07-21 13:28:53 +0800

    Split Up Components(but with some mistakes)

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
         firstName: 'Sheng',
         lastName: 'Yang',
     };
-    const { list, searchTerm} = this.state;
+    const { list, searchTerm } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -55,30 +55,82 @@ class App extends Component {
         </header>
 
         <h2>{ helloWorld }</h2>
-        <p>{username.firstName} {username.lastName}</p>
-        <form>
-          <input type="text"
-                 onChange={this.onSearchChange} />
-        </form>
-        {
-          list.filter(isSearched(searchTerm)).map(item =>
-              <div key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span>
-                  <button
-                    onClick={() => this.onDismiss(item.objectID)}
-                    type="button"
-                  >
-                    Dismiss
-                  </button>
-                </span>
-              </div>
-          )}
+        <p>{ username.firstName } { username.lastName }</p>
+        <Search
+          value={ searchTerm }
+          onchange={this.onSearchChange}
+        />
+        <Table
+          list={ list }
+          patter={ searchTerm }
+          onDismiss={ this.onDismiss }
+        />
+        {/*<form>*/}
+          {/*<input type="text"*/}
+                 {/*onChange={this.onSearchChange} />*/}
+        {/*</form>*/}
+        {/*{*/}
+          {/*list.filter(isSearched(searchTerm)).map(item =>*/}
+              {/*<div key={ item.objectID }>*/}
+                {/*<span>*/}
+                  {/*<a href={ item.url }>{ item.title }</a>*/}
+                {/*</span>*/}
+                {/*<span>{ item.author }</span>*/}
+                {/*<span>{ item.num_comments }</span>*/}
+                {/*<span>{ item.points }</span>*/}
+                {/*<span>*/}
+                  {/*<button*/}
+                    {/*onClick={() => this.onDismiss(item.objectID)}*/}
+                    {/*type="button"*/}
+                  {/*>*/}
+                    {/*Dismiss*/}
+                  {/*</button>*/}
+                {/*</span>*/}
+              {/*</div>*/}
+          {/*)}*/}
+      </div>
+    );
+  }
+}
+
+class Search extends  Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+      );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        { list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={ item.url }>{ item.title }</a>
+            </span>
+            <span>{ item.author }</span>
+            <span>{ item.num_comments }</span>
+            <span>{ item.points }</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     );
   }
