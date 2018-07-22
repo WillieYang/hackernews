19c5b25cd71c6d9cbcbdc019cb39c65e1b999b0d
WillieYang <849346604@qq.com>
2018-07-22 09:01:28 +0800

    inline style in React

--- src/App.js
+++ src/App.js
@@ -81,22 +81,29 @@ const Search = ({ value, onChange, children}) =>
   </form>
 
 const Table = ({ list, pattern, onDismiss }) =>
-  <div>
+  <div className="table">
     { list.filter(isSearched(pattern)).map(item =>
-      <div key={item.objectID}>
-            <span>
-              <a href={ item.url }>{ item.title }</a>
-            </span>
-        <span>{ item.author }</span>
-        <span>{ item.num_comments }</span>
-        <span>{ item.points }</span>
-        <span>
-              <Button
-                onClick={() => onDismiss(item.objectID)}
-              >
-                Dismiss
-              </Button>
-            </span>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
+          <a href={ item.url }>{ item.title }</a>
+        </span>
+        <span style={{ width: '30%' }}>
+          { item.author }
+          </span>
+        <span style={{ width: '10%' }}>
+          { item.num_comments }
+          </span>
+        <span style={{ width: '10%' }}>
+          { item.points }
+          </span>
+        <span style={{ width: '10%' }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
       </div>
     )}
   </div>
