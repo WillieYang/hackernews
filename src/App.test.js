7c81bb22013f3e663ed5e21b8c9937f636b358d5
WillieYang <849346604@qq.com>
2018-08-05 11:51:11 +0800

    Add a snapshot test by using test() function and react-test-renderer package

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+test('has a valid snapshot', () => {
+  const component = renderer.create(
+    <App />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
