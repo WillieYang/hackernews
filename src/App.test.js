ec26603523c5884b642e268c592c40fa00edb0e1
WillieYang <849346604@qq.com>
2018-08-11 23:29:54 +0800

    Add test and snapshot test for search, table, button component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './App';
+import App, { Search, Button, Table } from './App';
 
 describe('App', () => {
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
@@ -13,6 +14,61 @@ describe('App', () => {
   test('has a valid snapshot', () => {
     const component = renderer.create(
       <App />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Search', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<search>Search</search>, div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <search>Search</search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Button', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Give me More</Button>, div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Button>Give me More</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y'},
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z'},
+    ],
+  };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table {...props} />, div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Table {...props} />
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
